c42b5c7496b489720b8250e608ce17e23b53a5db
Chai <chai@fy.com>
2019-09-04 10:24:13 +0800

    会议类型

--- js/index.js
+++ js/index.js
@@ -1,67 +1,114 @@
 var paramsObj = getRequestParams();
 var ygbm = paramsObj.ygbm || "";
-var loadFlow = function (ygbm) {
-    layui.use('flow', function(){
-        // var $ = layui.jquery; //不用额外加载jQuery，flow模块本身是有依赖jQuery的，直接用即可。
-        var flow = layui.flow;
-        flow.load({
-            elem: '#messLists',
-            done: function (page, next) { //到达临界点（默认滚动触发），触发下一页
-                var lis = [];
-                //以jQuery的Ajax请求为例，请求下一页数据（注意：page是从2开始返回）
-                $.ajax({
-                    type: "post",
-                    url: "https://wx.hongyancloud.com/api/meeting/list",
-                    data: {
-                        pageNumber: page,
-                        pageSize: 20,
-                        ygbm: ygbm
-                    },
-                    success: function (res) {
-                        layui.each(res.data.list, function (i, o) {
-                            var str = " <li hyid='"+o.hyid+"'>\n" +
-                                "      <div class=\"messInfo\">\n" +
-                                "      <p>" + o.hytitle + "</p>\n" ;
-                            if(o.state=="1"){ // 未回执
-                                str+="       <span class=\"messReply\">"+o.hzzt+"</span>\n";
-                            }else if(o.state=="0"){ // 未查看
-                                str+="       <span class=\"messReply redCircle\"></span>\n";
-                            }else if(o.state=="2"){ // 参加
-                                str+="       <span class=\"messReply yue\">"+o.hzzt+"</span>\n";
-                            }else if(o.state=="3"){ // 不参加
-                                str+="       <span class=\"messReply dd_red\">"+o.hzzt+"</span>\n";
-                            };
-                            str+=" <time>" + o.kssj + "</time>" +
-                                "       </div>\n" +
-                                "      </li>"
-                            lis.push(str);
-                        });
-                        next(lis.join(''), page < res.data.totalPage);
-                        $("#messLists li").on("click",function () {
-                            var hyid = $(this).attr("hyid");
-                            if (dd){
-                                document.location.href = "meetingDetail_dd.html?hyid="+hyid+"&ygbm="+ygbm
+var page = 0;
+var pageSize = 10;
+$(function () {
+if(ygbm){
+    // miniRefresh 对象
+    var miniRefresh = new MiniRefresh({
+        container: '#minirefresh',
+        down: {
+            callback: function () {
+                doAjax('down');
+            }
+        },
+        up: {
+            isAuto: true,
+            callback: function () {
+                doAjax('up');
+            }
+        }
+    });
+    var doAjax = function (downOrUp) {
+        if (downOrUp == 'down') {
+            page = 1;// 下拉刷新页码设置
+            pageSize = 10;
+        } else {
+            page++;// 上拉加载递增页码
+            pageSize = 10;//
+        }
+
+        $.ajax({
+            url: 'https://wx.hongyancloud.com/api/meeting/list',// 请求网址
+            type: 'post',
+            data: {// 请求参数，一般会带上页码
+                pageNumber: page,
+                pageSize: pageSize,
+                ygbm: ygbm
+                /* 't': new Date().getTime()// 防止GET请求缓存 */
+            },
+            success: function (res) {
+                // 下面这段请根据自己的数据结构来处理
+                var arrLen = res.data.list.length;
+                if (arrLen > 0) {
+                    var str = '';
+                    $.each(res.data.list, function (i, o) {
+                        str += " <li hyid='" + o.hyid + "'>\n" +
+                            "      <div class=\"messInfo\">\n" +
+                            "      <p>" + o.hytitle + "</p>\n";
+                        if (o.state == "1") { // 未回执
+                            str += "       <span class=\"messReply\">" + o.hzzt + "</span>\n";
+                        } else if (o.state == "0") { // 未查看
+                            str += "       <span class=\"messReply redCircle\"></span>\n";
+                        } else if (o.state == "2") { // 参加
+                            str += "       <span class=\"messReply yue\">" + o.hzzt + "</span>\n";
+                        } else if (o.state == "3") { // 不参加
+                            str += "       <span class=\"messReply dd_red\">" + o.hzzt + "</span>\n";
+                        };
+                        str += " <time>" + o.kssj + "</time>" +
+                            "       </div>\n" +
+                            "      </li>";
+                    });
+                    setTimeout(function () { // 使用 setTimeout 函数是方便演示的，你可以不用 setTimeout 函数
+                        if (downOrUp == 'down') {
+                            $('#messLists').html('');
+                            $('#messLists').html(str);// DOM 插入数据
+                            $("#messLists li").on("click",function () {
+                                var hyid = $(this).attr("hyid");
+                                if (dd){
+                                    document.location.href = "meetingDetail_dd.html?hyid="+hyid+"&ygbm="+ygbm
+                                } else {
+                                    document.location.href = "meetingDetail.html?hyid="+hyid+"&ygbm="+ygbm
+                                }
+                            })
+                            miniRefresh.endDownLoading(true);// 结束下拉刷新
+                        } else {
+                            $('#messLists').append(str);
+                            $("#messLists li").on("click",function () {
+                                var hyid = $(this).attr("hyid");
+                                if (dd){
+                                    document.location.href = "meetingDetail_dd.html?hyid="+hyid+"&ygbm="+ygbm
+                                } else {
+                                    document.location.href = "meetingDetail.html?hyid="+hyid+"&ygbm="+ygbm
+                                }
+                            })
+                            if (res.data.totalpage == page) {// 是否已取完数据页
+                                miniRefresh.endUpLoading(true);// 结束上拉加载
                             } else {
-                                document.location.href = "meetingDetail.html?hyid="+hyid+"&ygbm="+ygbm
+                                miniRefresh.endUpLoading(false);
                             }
-                        })
-                        /*$(document).on("click touchend","#messLists li",function (e) {
-                            e.preventDefault();
-                            var hyid = $(this).attr("hyid");
-                            if (dd){
-                                document.location.href = "meetingDetail_dd.html?hyid="+hyid+"&ygbm="+ygbm
-                            } else {
-                                document.location.href = "meetingDetail.html?hyid="+hyid+"&ygbm="+ygbm
-                            }
-                        })*/
+                        }
+                    }, 600);
+
+                } else {
+                    if (downOrUp == 'down') {
+                        $('#messLists').html('');
+                        miniRefresh.endDownLoading(true);
+                    } else {
+                        miniRefresh.endUpLoading(true);
                     }
-                })
+                }
+            },
+            error: function () {
+                if (downOrUp == 'down') {
+                    $('#listdata').html('');
+                    miniRefresh.endDownLoading(true);
+                } else {
+                    miniRefresh.endUpLoading(true);
+                }
             }
         });
-    });
+    };
 }
-$(function () {
-    if(ygbm){
-        loadFlow(ygbm);
-}
+
 });
